5da2d6214db543ff7176500ed3752054c1e1ee07
EG <fangweiyi@gdu-tech.com>
2017-08-10 11:20:34 +0800

    Add category

---
+++ EGMapCollector/EGMapCollector/UIImage+Rotate.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Rotate.h
+//  EGMapCollector
+//
+//  Created by EG on 2017/8/10.
+//  Copyright © 2017年 EGMade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Rotate)
+
+- (UIImage*)rotateImageWithAngle:(CGFloat)angle;
+
+@end
---
+++ EGMapCollector/EGMapCollector/UIImage+Rotate.m
@@ -0,0 +1,34 @@
+//
+//  UIImage+Rotate.m
+//  EGMapCollector
+//
+//  Created by EG on 2017/8/10.
+//  Copyright © 2017年 EGMade. All rights reserved.
+//
+
+#import "UIImage+Rotate.h"
+
+@implementation UIImage (Rotate)
+
+- (UIImage*)rotateImageWithAngle:(CGFloat)angle {
+    CGFloat width = CGImageGetWidth(self.CGImage);
+    CGFloat height = CGImageGetHeight(self.CGImage);
+    
+    CGSize rotatedSize;
+    
+    rotatedSize.width = width;
+    rotatedSize.height = height;
+    
+    UIGraphicsBeginImageContext(rotatedSize);
+    CGContextRef bitmap = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(bitmap, rotatedSize.width/2, rotatedSize.height/2);
+    CGContextRotateCTM(bitmap, angle * M_PI / 180); //* M_PI / 180
+    CGContextRotateCTM(bitmap, M_PI);
+    CGContextScaleCTM(bitmap, -1.0, 1.0);
+    CGContextDrawImage(bitmap, CGRectMake(-rotatedSize.width/2, -rotatedSize.height/2, rotatedSize.width, rotatedSize.height), self.CGImage);
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+@end
